9b212f13e16786f6a5b9678580db76288dd489a9
viswanadh800 <viswanadh.800@gmail.com>
2024-02-20 00:02:15 +0530

    Removed Autowired in service and controller

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 @Service
 public class OrderService {
-    @Autowired
-    OrderRepository orderRepository;
+   // @Autowired
+    OrderRepository orderRepository=new OrderRepository();
 
     public void addOrder(Order order) {
         orderRepository.addOrder(order);
